75855b8a36e6f23ea935d876081593f4e579cf1c
Alexander Yonchev <19502@uktc-bg.com>
2023-04-19 11:52:07 +0300

    trotki

--- adVehicles.php
+++ adVehicles.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "123456789";
+$password = "fyre02";
 $dbname = "xiaomi";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -18,20 +18,19 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     echo "<div class='product-list'>";
     while ($row = $result->fetch_assoc()) {
-       
+        echo "<a href='product-details.php?id=" . $row["id"] . "' class='product-link'>"; // Add product link with product id as query parameter
         echo "<div class='product-item'>";
         echo "<h2>" . $row["title"] . "</h2>";
         echo "<img src='vehicles\\" . $row['pic'] . "' alt='" . $row['title'] . "' class='product-image'/>";
-        echo "<p>Motor power: " . $row["motorPower"] . "W</p>";
-        echo "<p>Top speed: " . $row["topSpeed"] . " km/h</p>";
-        echo "<p>Price: " . $row["price"] . " лв.</p>";
+        echo "<p class = 'card-info'>Motor power: " . $row["motorPower"] . "W</p>";
+        echo "<p class = 'card-info'>Top speed: " . $row["topSpeed"] . " km/h</p>";
+        echo "<p class = 'card-price'>Price: " . $row["price"] . " лв.</p>";
         echo "</div>";
-        
+        echo "</a>"; // Close product link
     }
     echo "</div>";
 } else {
     echo "No products found.";
 }
-
 $conn->close();
 ?>
