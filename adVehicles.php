4297dd8ac1ed08b62da396a737fd74249038f7e1
Alexander Yonchev <19502@uktc-bg.com>
2023-04-19 14:30:22 +0300

    da

--- adVehicles.php
+++ adVehicles.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "123456789";
+$password = "fyre02";
 $dbname = "xiaomi";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -18,7 +18,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     echo "<div class='product-list'>";
     while ($row = $result->fetch_assoc()) {
-        echo "<a href='products\product.html'" . $row["id"] . "' class='product-link'>"; // Add product link with product id as query parameter
+        echo "<a href='products\product.html'" . $row["id"] . "' class='product-link'>"; 
         echo "<div class='product-item'>";
         echo "<h2>" . $row["title"] . "</h2>";
         echo "<img src='vehicles\\" . $row['pic'] . "' alt='" . $row['title'] . "' class='product-image'/>";
@@ -26,7 +26,7 @@ if ($result->num_rows > 0) {
         echo "<p class = 'card-info'>Top speed: " . $row["topSpeed"] . " km/h</p>";
         echo "<p class = 'card-price'>Price: " . $row["price"] . " лв.</p>";
         echo "</div>";
-        echo "</a>"; // Close product link
+        echo "</a>"; 
     }
     echo "</div>";
 } else {
