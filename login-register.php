dab36b6d4a15a5f4e1d5e22f77c8770c59daee40
Alexander Yonchev <19502@uktc-bg.com>
2023-04-19 23:45:35 +0300

    login navbar

--- login-register.php
+++ login-register.php
@@ -8,76 +8,9 @@ require('log.php')
 
 <head>
     <title>Login and Register Form</title>
+    <link href="https://fonts.googleapis.com/css?family=Blinker" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <style>
-        body {
-            background-image: url("images/mi-bg.webp");
-            background-repeat: no-repeat;
-            background-size:cover;
-            background-position:center;
-            margin: 0;
-            padding: 0;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        .form-container {
-            background-color: rgb(0, 0, 0,0.5);
-            border-radius: 10px;
-            width: 300px;
-            margin: 0 auto;
-            padding: 20px;
-            margin-top: 13em;
-            text-align: center;
-            place-items:center;
-        }
-
-        .form-container h2 {
-            color: coral;
-            font-size: 28px;
-            margin-bottom: 20px;
-        }
-
-        .form-container input[type="text"],
-        .form-container input[type="password"],
-        .form-container input[type="email"] {
-            width: 100%;
-            padding: 12px;
-            margin-top: 8px;
-            margin-bottom: 20px;
-            border: none;
-            border-radius: 5px;
-            background-color: #f6f6f6;
-            color: #333;
-        }
-
-        .form-container input[type="text"]::placeholder,
-        .form-container input[type="password"]::placeholder,
-        .form-container input[type="email"]::placeholder {
-            color: #999;
-        }
-
-        .form-container input[type="submit"] {
-            background-color: coral;
-            color: #fff;
-            padding: 12px 20px;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            font-size: 16px;
-            font-weight: bold;
-            transition: background-color 0.3s ease;
-        }
-
-        .form-container input[type="submit"]:hover {
-            background-color: #ff9800;
-        }
-
-        .form-container .switch-form {
-            color: coral;
-            cursor: pointer;
-            font-size: 14px;
-        }
-    </style>
+    <link rel="stylesheet" href="login-register.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         $(document).ready(function() {
@@ -108,7 +41,7 @@ require('log.php')
             <input type="password" name="password" maxlength="10" placeholder="Password">
             <input type="submit" name="submit1" value="Login">
         </form>
-        <p class="switch-form" id="btn-register">Don't have an account? Register</p>
+        <p class="switch-form" id="btn-register">Don't have an account? <mark class = "marked" >Register</mark></p>
     </div>
 
     <div class="form-container" id="register-form">
@@ -120,7 +53,7 @@ require('log.php')
             <input type="password" name="password"  placeholder="Password">
             <input type="submit" name="submit" value="Register">
         </form>
-        <p class="switch-form" id="btn-login">Already have an account? Login</p>
+        <p class="switch-form" id="btn-login">Already have an account? <mark class = "marked" >Login</mark></p>
     </div>
-        </body>
-        </html>
+</body>
+</html>
