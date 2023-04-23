b0601eeadaed1839499823e3cba5e235ac8147b2
Alexander Yonchev <19502@uktc-bg.com>
2023-04-23 20:29:47 +0300

    opraven responsive login i register

--- login-register.php
+++ login-register.php
@@ -1,5 +1,5 @@
 <?php
-require('nav.html');
+
 require('reg.php');
 require('log.php')
 
@@ -33,27 +33,32 @@ require('log.php')
 </head>
 
 <body>
+
+<?php
+    require('nav.html');
+?>
     <div class="login-form">
     <div class="form-container" id="login-form">
         <h2>Login</h2>
         <form method="post">
-            <input type="text" name="email" maxlength="10" placeholder="Email">
-            <input type="password" name="password" maxlength="10" placeholder="Password">
+            <input type="text" name="email" maxlength="30" placeholder="Email">
+            <input type="password" name="password" maxlength="30" placeholder="Password">
             <input type="submit" name="submit1" value="Login">
         </form>
         <p class="switch-form" id="btn-register">Don't have an account? <mark class = "marked" >Register</mark></p>
     </div>
-</div>
+
     <div class="form-container" id="register-form">
         <h2>Register</h2>
         <form method="post">
-            <input type="text" name="fname" maxlength="10" placeholder="First Name">
-            <input type="text" name="lname" maxlength="10" placeholder="Last Name">
-            <input type="email" name="email" placeholder="Email">
+            <input type="text" name="fname" maxlength="30" placeholder="First Name">
+            <input type="text" name="lname" maxlength="30" placeholder="Last Name">
+            <input type="email" name="email" maxlength="30"placeholder="Email">
             <input type="password" name="password"  placeholder="Password">
             <input type="submit" name="submit" value="Register">
         </form>
         <p class="switch-form" id="btn-login">Already have an account? <mark class = "marked" >Login</mark></p>
     </div>
+</div>
 </body>
 </html>
