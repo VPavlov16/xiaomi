12cee5bd9c2ed09b1196d8cd5a6f18d38e11c976
Venelin Pavlov <boromazniq@gmail.com>
2023-04-25 10:51:24 +0300

    devices

---
+++ adSmdev.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "123456789";
+$dbname = "xiaomi";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM smart_devices";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<div class='product-list'>";
+    while ($row = $result->fetch_assoc()) {
+       // echo "<a href='product-details.php?id=" . $row["id"] . "' class='product-link'>"; 
+        echo "<div class='product-item'>";
+        echo "<h2>" . $row["Model"] . "</h2>";
+        echo "<img src='smart devices\\" . $row['pic'] . "' alt='" . $row['Model'] . "' class='product-image'/>";
+        echo "<p class = 'card-info'>Suction Power: " . $row["suction_power"] . " pA</p>";
+        echo "<p class = 'card-price'>Price: " . $row["price"] . " лв.</p>";
+        echo "<button class='button button2'>Add to cart</button>";
+        echo "</div>";
+        echo "</a>";
+    }
+    echo "</div>";
+} else {
+    echo "No products found.";
+}
+$conn->close();
+?>
--- smdev.php
+++ smdev.php
@@ -1,17 +1,13 @@
-<?php
-include('nav.html');
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Products</title>
+    <link rel="stylesheet" type="text/css" href="vehstyle.css">
 </head>
 <body>
-    
+<?php
+include('nav.html');
+include('adSmdev.php');
+?>
 </body>
 </html>
