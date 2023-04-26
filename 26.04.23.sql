154c975b663e1aebad13b3262a128ccb1e87abe6
Venelin Pavlov <boromazniq@gmail.com>
2023-04-26 10:59:36 +0300

    db

--- 26.04.23.sql
+++ 26.04.23.sql
@@ -194,8 +194,9 @@ CREATE TABLE `registers` (
   `lname` varchar(45) NOT NULL DEFAULT 'a',
   `email` varchar(45) NOT NULL DEFAULT 'a',
   `pass` varchar(255) NOT NULL DEFAULT 'a1',
+  `type` varchar(45) NOT NULL DEFAULT 'user',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -204,7 +205,7 @@ CREATE TABLE `registers` (
 
 LOCK TABLES `registers` WRITE;
 /*!40000 ALTER TABLE `registers` DISABLE KEYS */;
-INSERT INTO `registers` VALUES (7,'petko','asd','imi@asd','03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4'),(8,'Registriram se','Ot telefona','tuieottelanaveni@abv.bg','4a31b3a94f8b9446588917a9e7aaa11bb13ee52d63136678623bb1667e8c1b77'),(9,'Venelin e gei','❤️❤️❤️❤️❤️????','dhdhf@abv.bg','932679c1574b5b1e8376929acad78bac544fde36f2b7eb10f463d971d904885e'),(10,'Venelin','Pavlov','edno@abv.bg','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3'),(11,'a','s','imi@abv.bg','b3440b114d53365b37df79cacfe85af6f327faf074efd6d05a8940a427f4180c');
+INSERT INTO `registers` VALUES (7,'petko','asd','imi@asd','03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4','user'),(8,'Registriram se','Ot telefona','tuieottelanaveni@abv.bg','4a31b3a94f8b9446588917a9e7aaa11bb13ee52d63136678623bb1667e8c1b77','user'),(9,'Venelin e gei','❤️❤️❤️❤️❤️????','dhdhf@abv.bg','932679c1574b5b1e8376929acad78bac544fde36f2b7eb10f463d971d904885e','user'),(10,'Venelin','Pavlov','edno@abv.bg','a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','admin'),(11,'a','s','imi@abv.bg','b3440b114d53365b37df79cacfe85af6f327faf074efd6d05a8940a427f4180c','user'),(12,'EMilka','Pilka','imeilche@abv.bg','329d3c818a604b1927a79b53fcac76d952ff4cfec42dbc020cd30d6fc0c38b70','user');
 /*!40000 ALTER TABLE `registers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -309,4 +310,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-26  9:12:21
+-- Dump completed on 2023-04-26 10:59:11
