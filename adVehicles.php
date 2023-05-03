aa840be1a6135930870b4b02303cd2782bd449b2
Venelin Pavlov <boromazniq@gmail.com>
2023-05-03 13:01:18 +0300

    panek

--- adVehicles.php
+++ adVehicles.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "fyre02";
+$password = "123456789";
 $dbname = "xiaomi";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
