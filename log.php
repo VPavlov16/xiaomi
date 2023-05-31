dba3697aa78c8ffba37489cd3b9843827600f87b
Venelin Pavlov <boromazniq@gmail.com>
2023-05-31 21:02:12 +0300

    remember me

--- log.php
+++ log.php
@@ -2,7 +2,7 @@
 session_start();
 $servername = "localhost";
 $username = "root";
-$password = "fyre02";
+$password = "123456789";
 $database = "xiaomi";
 $status = "none";
 
@@ -26,6 +26,13 @@ if ( isset( $_POST['submit1'] ) ) {
 	if ( $userInfo ) {
 		$_SESSION['user'] = [$userInfo[0]['id'],$userInfo[0]['fname'],$userInfo[0]['lname'],$userInfo[0]['email'],$userInfo[0]['type'],$userInfo[0]['cart']];
 		$status = "none";
+		if (isset($_POST['remember_me'])) {
+			$token = bin2hex(random_bytes(16));
+			setcookie('remember_token', $token, time() + 604800); // Expiry time: 1 week
+	  
+			$stmt2 = $connection->prepare("UPDATE registers SET token = ? WHERE email = ?");
+			$stmt2->execute([$token, $email]);
+		  }
 	
 		header("location: home.php");
 		exit;
