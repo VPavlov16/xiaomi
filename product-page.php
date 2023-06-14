72395cd5500137eb31f62791557d14824d1ef6a0
Alexander Yonchev <19502@uktc-bg.com>
2023-06-14 23:54:19 +0300

    csssss

--- product-page.php
+++ product-page.php
@@ -56,6 +56,7 @@ $WearCSS = "";
     </style>
 </head>
 <body>
+    
     <?php
         require('nav.php');
     ?>
@@ -92,16 +93,16 @@ $WearCSS = "";
         </div>
 
 
-            <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+       <!-- <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active" data-bs-interval="10000">
-                <img src="<?php echo $folder . '/' . $cover ?>" class="d-block w-100" alt="...">
+                <img src="..." class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item" data-bs-interval="2000">
-                <img src="<?php echo $folder . '/' . $pic1 ?>" class="d-block w-100" alt="...">
+                <img src="..." class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                <img src="<?php echo $folder . '/' . $pic2 ?>" class="d-block w-100" alt="...">
+                <img src="..." class="d-block w-100" alt="...">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
@@ -112,9 +113,9 @@ $WearCSS = "";
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
-            </div>
+        </div>
 
-
+    -->
 
             <div class="product-details">
                 <div class="product-details-inner">
@@ -173,7 +174,6 @@ $WearCSS = "";
                 </div>
             </div>
     </div>
-
 
 
 <script>
